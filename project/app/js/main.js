4bf5e2525ec1b080738b46c95c67e343729db428
byio <ben@benyang.io>
2018-07-28 16:16:45 +0800

    create saveEventDataLocally function that takes an array of event objects and adds them to the indexedDB database

--- project/app/js/main.js
+++ project/app/js/main.js
@@ -14,6 +14,21 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+function saveEventDataLocally(events) {
+  if (!('indexedDB' in window)) {
+    return null;
+  }
+  return dbPromise.then(db => {
+    const tx = db.transaction('events', 'readwrite');
+    const store = tx.objectStore('events');
+    return Promise.all(events.map(event => store.put(event)))
+      .catch(() => {
+        tx.abort();
+        throw Error('Events were not added to the store.');
+      });
+  });
+}
+
 function createIndexedDB() {
   if (!('indexedDB' in window)) {
     return null;
